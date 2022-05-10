20a7ca446b7f64f2ecacc5d47804deb28e62acd6
aproxya <apisani_96@outlook.com>
2022-05-10 15:00:42 -0400

    add - imports; modified - application structure

--- src/Application.js
+++ src/Application.js
@@ -1,7 +1,29 @@
+import React, { useState } from 'react'
+
+// library imports.
+import styled from 'styled-components'
+// ----------------
+
+// component imports.
+import Splash from './components/splash'
+// ------------------
+
+// styled components.
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+`
+// ------------------
+
+
+
 const Application = () => {
   return (
-    <div>
-    </div>
+    <AppContainer>
+      <Splash />
+    </AppContainer>
   )
 }
 
